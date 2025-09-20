b693b9b6c4c9ab220a09ca852fd7db6a8d8163f9
Ricardo Morales <ricsf2000@gmail.com>
2025-09-20 00:18:22 -0500

    added some additional notes for todo's

--- src/app/admin/volunteer-matching/page.tsx
+++ src/app/admin/volunteer-matching/page.tsx
@@ -7,6 +7,7 @@ export default function AdminVolunteerMatching() {
         <p>TODO: Implement this - Eb</p>
         <p>Volunteer Name (Make dummy API call and autofill)</p>
         <p>Matched Event (Make dummy API call and autofill)</p>
+        <p>Am thinking both these fields should be dropdown menus to select but also there is a button that autofills with the API calls</p>
       </div>
     </div>
   );
