51a8d61e45c3cf60e12de659edfce531cb03b0ac
ebrahim-vejlani <e1.vejlani@gmail.com>
2025-09-21 13:57:44 -0500

    First take at volunteer matching UI

--- src/app/admin/volunteer-matching/page.tsx
+++ src/app/admin/volunteer-matching/page.tsx
@@ -1,14 +1,10 @@
-export default function AdminVolunteerMatching() {
+import VolunteerMatchingForm from '@/components/volunteer-matching';
+
+export default function VolunteerMatchingPage() 
+{
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-6">Volunteer Matching</h1>
-      
-      <div className="space-y-4">
-        <p>TODO: Implement this - Eb</p>
-        <p>Volunteer Name (Make dummy API call and autofill)</p>
-        <p>Matched Event (Make dummy API call and autofill)</p>
-        <p>Am thinking both these fields should be dropdown menus to select but also there is a button that autofills with the API calls</p>
-      </div>
+      <VolunteerMatchingForm />
     </div>
   );
 }
